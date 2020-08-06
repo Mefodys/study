eac91212f70898e6820ae16c17245c92d825e5cc
Mefodys <mefody123@gmail.com>
2020-08-06 21:05:52 +0300

    added the second file with password. opened this file in utf8

--- python_automate/2_password_file/password_file.py
+++ python_automate/2_password_file/password_file.py
@@ -1,13 +1,13 @@
-#test
-
 #1 straight open of the file
 print ("----------")
 print ("straight open of the file:")
 password = open("C:\py\study\python_automate\\2_password_file\secretpass.txt","r")  #used double slash (!)
+second_password = open("C:\py\study\python_automate\\2_password_file\\file_with_password.txt","r")  #used double slash (!)
 
 print ("file object in python -> ", password)
 print ("readed file with password -> ", password.read())
-
+print ("file object in python -> ", second_password)
+print ("readed file with password -> ", second_password.read())
 
 
 
@@ -17,3 +17,12 @@ print ("open the file using with expression:")
 with open("C:\py\study\python_automate\\2_password_file\secretpass.txt", "r") as f:
     data = f.read()
 print(data)
+
+
+
+#3 open file in UTF8
+print ("----------")
+import io
+f = io.open("C:\py\study\python_automate\\2_password_file\\file_with_password.txt", mode="r", encoding="utf-8")
+print ("file object in python -> ", f)
+print ("readed file with password -> ", f.read())
