2426f6c0f0bcbd3232a43a68b5725ae5dd46bc9f
Mefodys <mefody123@gmail.com>
2020-08-06 21:40:07 +0300

    added 4th section - open file with password, compare its md5 with hardcoded md5

--- python_automate/2_password_file/password_file.py
+++ python_automate/2_password_file/password_file.py
@@ -1,6 +1,6 @@
 #1 straight open of the file
 print ("----------")
-print ("straight open of the file:")
+print ("#1 straight open of the file:")
 password = open("C:\py\study\python_automate\\2_password_file\secretpass.txt","r")  #used double slash (!)
 second_password = open("C:\py\study\python_automate\\2_password_file\\file_with_password.txt","r")  #used double slash (!)
 
@@ -13,7 +13,7 @@ print ("readed file with password -> ", second_password.read())
 
 #2 open the file using with expression
 print ("----------")
-print ("open the file using with expression:")
+print ("#2 open the file using with expression:")
 with open("C:\py\study\python_automate\\2_password_file\secretpass.txt", "r") as f:
     data = f.read()
 print(data)
@@ -22,7 +22,34 @@ print(data)
 
 #3 open file in UTF8
 print ("----------")
+print ("#3 open file in UTF8")
 import io
 f = io.open("C:\py\study\python_automate\\2_password_file\\file_with_password.txt", mode="r", encoding="utf-8")
 print ("file object in python -> ", f)
 print ("readed file with password -> ", f.read())
+
+
+#4 access control. compareing md5
+print ("----------")
+print ("#4 access control. comparing md5")
+import hashlib 
+md5 = "3fc0a7acf087f549ac2b266baf94b8b1" 
+
+password = open("C:\py\study\python_automate\\2_password_file\secretpass.txt","r")
+read_password = password.read()
+ 
+# encoding qwerty123 using encode() 
+# then sending to md5() 
+result = hashlib.md5(read_password.encode()) 
+  
+# printing the equivalent hexadecimal value. 
+print("The hexadecimal equivalent of hash is : ", result.hexdigest()) 
+print() 
+print("md5 var type", md5, " ", type(md5))
+print("result var type", result," ", type(result))
+print(type(result.hexdigest())) 
+print() 
+if result.hexdigest() == md5: 
+    print("Access allowed")
+else:
+    print("Access denied")
