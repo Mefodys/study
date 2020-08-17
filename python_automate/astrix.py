71eeeb8f8336845530c58c0c52438348ce8a4dd5
unknown <morker@ya.ru>
2020-08-17 21:47:13 +0300

    astrix works

--- python_automate/astrix.py
+++ python_automate/astrix.py
@@ -11,8 +11,15 @@ import time
 # time.sleep(1)
 
 a=['-','/','|','\\']
-for i in range(len(a)):
-    print(a[i])
-    time.sleep(1)
+while 1: 
+    for i in range(len(a)):
+        print(a[i], end="\r")
+        time.sleep(0.1)
     
-    
+# animation = "|/-\\"
+# idx = 0
+# while (1):
+#     a=animation[idx % len(animation)]
+#     print(a, end="\r")
+#     idx += 1
+#     time.sleep(0.1)
