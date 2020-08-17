2e29371e3dcf195bcb35cca495f94b6586ca6d0f
unknown <morker@ya.ru>
2020-08-17 20:40:39 +0300

    added 4 py file with input operations + added sep example to 3 py file

--- python_automate/3_operations/#3 simple operations.py
+++ python_automate/3_operations/#3 simple operations.py
@@ -8,5 +8,4 @@ print ('22 / 7 =',22 / 3)
 print ('22.0 / 7.0 = ' ,22.0 / 3)
 print ("'Alice' + ' and ' + 'Bob = '",'Alice' + ' and ' + 'Bob')
 print ("'Alice' * 3 =", 'Alice' * 3)
-
-
+print('1234','5678','', sep='-')
