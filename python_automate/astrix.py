518e5fe6f5e901d41d129df7f4f37b75e6aae0d8
unknown <morker@ya.ru>
2020-08-17 21:13:37 +0300

    added 5_astrix.py

---
+++ python_automate/astrix.py
@@ -0,0 +1,18 @@
+#while (1):
+import time
+
+# print ('-')
+# time.sleep(1)
+# print ('/')
+# time.sleep(1)
+# print ('|')
+# time.sleep(1)
+# print ('\\')
+# time.sleep(1)
+
+a=['-','/','|','\\']
+for i in range(len(a)):
+    print(a[i])
+    time.sleep(1)
+    
+    
