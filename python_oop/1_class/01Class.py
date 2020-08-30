10b5b667a257204dc5f7b25a0d1c75e2215210bb
Mefodys <mefody123@gmail.com>
2020-08-30 18:40:55 +0300

    classes_study

---
+++ python_oop/1_class/01Class.py
@@ -0,0 +1,30 @@
+#object - экземпляр класса
+
+a= [1,2,3]
+print(type(a))
+
+print(isinstance(a, list))
+print(isinstance(a, type))
+
+print(type(list))
+print(type(type))
+
+class Car:
+    model = "Skoda"
+    engine = 1.8
+
+a = Car()
+print(type(a))
+
+# Аттрибуты класса
+
+class Person:
+    name = "Anton"
+    age = 30
+
+
+newPerson = Person()
+print (newPerson.name)
+print (newPerson.age)
+
+print (Person.__dict__)
