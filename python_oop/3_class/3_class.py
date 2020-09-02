468a47584ed204d8f7060e6e3ea90a0b56207fc2
Mefodys <mefody123@gmail.com>
2020-09-02 20:25:55 +0300

    class Cat and Dog. method __init__

---
+++ python_oop/3_class/3_class.py
@@ -0,0 +1,31 @@
+#self это параметр, который указывает на обьект.
+#вывод self из метода показывает, что он указывает на созданный объект.
+
+#метод __init__ инициализирует объект с параматрами, которые нам нужны. 
+
+class Cat:
+
+    def __init__(self):
+        print('Hello, new object is', self)
+
+
+
+tom = Cat()
+print (tom)
+
+print('\n')
+jerry = Cat()
+print(jerry)
+
+
+print('\n===\nclass Dog\n')
+
+
+class Dog:
+
+    def __init__(self, name, age, color):
+        print('Name is', name)
+        print('Age is', age)
+        print('Color is',  color)
+
+Chelsi = Dog('Chelsi', 10, 'black')
