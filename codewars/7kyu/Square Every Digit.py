4cc590a7fa5ed8adbfea45e0d702d728b70ed707
Anton Mefodys <mefody123@gmail.com>
2020-10-06 18:51:54 +0300

    7kyu square every digit. added more solutions

--- codewars/7kyu/Square Every Digit.py
+++ codewars/7kyu/Square Every Digit.py
@@ -1,16 +1,28 @@
 def square_digits(num):
-    res = [(int(x)) ** 2 for x in str(num)]
-    print(res)
-    s = [str(i) for i in res]
-    t = "".join(s)
+    res = [(int(x)) ** 2 for x in str(num)] # [81, 1, 1, 81]
+    return int("".join([str(i) for i in res]))
 
-    print(int(t))
 
 #SMARTER SOLUTION:
 # def square_digits(num):
 #     ret = ""
 #     for x in str(num):
-#         ret += str(int(x)**2)
-#     return int(ret)
+#         ret += str(int(x) ** 2)
+#     # return int(ret)
+
+#ANOTHER SOLUTION:
+# def square_digits(num):
+#     return int(''.join(str(int(c)**2) for c in str(num)))
 
 square_digits(9119)
+
+a=1
+b="abcde"
+
+print(dir(a))
+print(dir(b))
+
+print(str(a).join(b))
+
+c = str(a) + b
+print(c)
