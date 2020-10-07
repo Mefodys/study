99b1f4cd1f888d36ae094ca4291bd30c431bd064
Anton Mefodys <mefody123@gmail.com>
2020-10-07 18:19:59 +0300

    6 kyu count characters in your sting

---
+++ codewars/6kyu/Count characters in your string.py
@@ -0,0 +1,15 @@
+# 6 kyu
+# Count characters in your string
+
+"""
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+What if the string is empty? Then the result should be empty object literal, {}.
+"""
+
+def count(string):
+    # The function code should be here
+    return {}
+
+
+count('aba')   #{'a': 2, 'b': 1}
+count('')      #{}
