765f3e2ea8f83e7475b0b0722a443d15966b2dcf
Anton Mefodys <mefody123@gmail.com>
2020-10-08 22:13:35 +0300

    7kyu. 1-2-3

--- codewars/7kyu/Testing 1-2-3.py
+++ codewars/7kyu/Testing 1-2-3.py
@@ -14,12 +14,46 @@ number([]) # => []
 number(["a", "b", "c"]) # => ["1: a", "2: b", "3: c"]
 """
 
+
+# def number(lines):
+#     count = 0
+#     r = {}
+#
+#     for i in lines:
+#         count += 1
+#         r[count] = i
+#     print(r)
+#     for i in r:
+#         print(i,r[i])
+#     print(t)
+
+
+#WORKS!
+# def number(lines):
+#     count = 0
+#     temp_string = ""
+#     temp_list = []
+#
+#     for i in lines:
+#         count += 1
+#         temp_string = str(count) + ": " + i
+#         temp_list.append(temp_string)
+#
+#     print(temp_list)
+
+#Shorter variant
 def number(lines):
     count = 0
-    r = []
+    temp_list = []
+
     for i in lines:
         count += 1
-        print (count, ":" , i)
+        temp_list.append(str(count) + ": " + i)
 
-number([])   #, []
+    print(temp_list)
+
+
+
+
+number([])  # , []
 number(["a", "b", "c"])  # ["1: a", "2: b", "3: c"]
