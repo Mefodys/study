84f0cda63c030b9538687218bb78a89377136030
Anton Mefodys <mefody123@gmail.com>
2020-10-08 17:39:03 +0300

    6kyu. count characters inyour string

--- codewars/6kyu/Count characters in your string.py
+++ codewars/6kyu/Count characters in your string.py
@@ -7,9 +7,15 @@ What if the string is empty? Then the result should be empty object literal, {}.
 """
 
 def count(string):
-    # The function code should be here
-    return {}
+    d = {}
+    for i in string:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    print(d)
 
 
-count('aba')   #{'a': 2, 'b': 1}
-count('')      #{}
+
+count('aba')      #{'a': 2, 'b': 1}
+# count('')      #{}
