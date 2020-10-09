5dadc7665329602febd328f0e74766ff13a2cce8
Anton Mefodys <mefody123@gmail.com>
2020-10-09 23:18:14 +0300

    6kyu. Directions.done  + smart solution

--- codewars/5kyu/Directions.py
+++ codewars/5kyu/Directions.py
@@ -120,3 +120,11 @@ def dirReduc(arr):
 a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
 b = ["NORTH", "WEST", "SOUTH", "EAST"]
 dirReduc(a)  # ['WEST']
+dirReduc(b)  # ["NORTH", "WEST", "SOUTH", "EAST"])
+
+#SMART SOLUTION!!!!
+# def dirReduc(arr):
+#     dir = " ".join(arr)
+#     dir2 = dir.replace("NORTH SOUTH",'').replace("SOUTH NORTH",'').replace("EAST WEST",'').replace("WEST EAST",'')
+#     dir3 = dir2.split()
+#     return dirReduc(dir3) if len(dir3) < len(arr) else dir3
