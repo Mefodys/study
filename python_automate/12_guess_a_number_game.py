b96801ec34446eaa93a6d5ce6d53d691c941a63b
Mefodys <mefody123@gmail.com>
2020-10-17 15:53:28 +0300

    added attempts counter

--- python_automate/12_guess_a_number_game.py
+++ python_automate/12_guess_a_number_game.py
@@ -1,25 +1,34 @@
 import random
 
 number = random.randint(1, 20)
+attempts_number = 0
+attempts_limit = 5
 
 while True:
-    x = input('Make a guess of a number from 1 to 20. Type it and press Enter: ')
-    if x != None:
+    if attempts_limit == 0:
+        print('You have no more attempts. The number was ' + str(number) + '. THE GAME IS OVER')
+        break
+    else:
         try:
+            print('You have ' + str(attempts_limit) + ' attempts')
+            x = input('Make a guess of a number from 1 to 20. Type it and press Enter: ')
             x = int(x)
         except:
-            print ('you entered not a valid number (from 1 to 20)!')
-            break
-
-
-        if x < number:
-            print('The number is bigger than your guess. Try again!')
+            print('ERROR: You have entered not a valid number (from 1 to 20)! Try again. ')
             continue
-        elif x > number:
-            print('The number is smaller than your guess. Try again!')
-        elif x == number:
-            print('You are right!')
-            break
+
+    attempts_number += 1
+    attempts_limit -= 1
+
+    if x < number:
+        print('The number is bigger than your guess. Try again!')
+        continue
+    elif x > number:
+        print('The number is smaller than your guess. Try again!')
+    elif x == number:
+        print('YOU ARE RIGHT!')
+        print('Number of attempts: ', attempts_number)
+        break
     # else:
     #     print('some unkown error. contact devs team')
     #     continue
