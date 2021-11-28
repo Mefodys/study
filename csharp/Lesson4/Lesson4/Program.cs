6c1548c0f15e12993de6eb4da90148485a0dc1fd
Anton Mefodys <mefody123@gmail.com>
2021-11-28 20:28:24 +0300

    4

---
+++ csharp/Lesson4/Lesson4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+ переменные
+объявление
+инициализация - когда данные пормещатся в перменную.
+
+//тип_данных имя_переменной;
+ 
+ */
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 15;
+            b = 10;
+            c = a + b;
+            //Console.WriteLine(a);
+            Console.WriteLine("a = " + a); 
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("a + b = " + c);
+
+            //плохо называть переменные так, как ниже
+            bool variable = false;
+            bool variable2 = true;
+            Console.WriteLine(variable);
+            Console.WriteLine(variable2);
+
+            char f = '#'; 
+            char g = '*';
+            string fda = "asdfjkasdf;ljk";
+            Console.WriteLine(f + g + fda);
+            Console.WriteLine(f);
+
+        
+
+
+        }
+    }
+}
