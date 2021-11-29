1071245e6229ed412d47aba0873b6457d7bb1838
Anton Mefodys <mefody123@gmail.com>
2021-11-29 20:04:38 +0300

    5

---
+++ csharp/lesson5/lesson5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ vvod dannih v console
+ */
+namespace lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
