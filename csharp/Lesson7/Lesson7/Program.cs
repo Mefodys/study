942aac61e1f902f7f1060c9ba38ea5e1d261b087
Anton Mefodys <mefody123@gmail.com>
2021-11-29 21:19:26 +0300

    7

---
+++ csharp/Lesson7/Lesson7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "7";
+            int a = int.Parse(str);
+
+
+            /*
+            string str2 = "5,6";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ",",
+            };
+            
+            double b = double.Parse(str2, numberFormatInfo);
+            */
+
+            string str3 = "5.9";
+            try
+            {
+                double c = double.Parse(str3);
+                Console.WriteLine(c);
+             }
+            catch (Exception)
+            {
+                Console.WriteLine("Convertation error");
+            }
+
+            string str4 = "2 h";
+            int aa;
+
+            bool result = int.TryParse(str4, out aa); //return bool
+            if (result)
+            {
+                Console.WriteLine("Success " + aa);
+            }
+            else
+            {
+                Console.WriteLine("Unsuccessful convert");
+            }
+        }
+    }
+}
