af4ec9c2140fb7154c93a05d4bfbee1f05d1ce0c
Anton Mefodys <mefody123@gmail.com>
2021-11-30 21:33:35 +0300

    10

---
+++ csharp/Lesson10/Lesson10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ *  несколько проектов в одном решении
+ *  
+ *  решение или solution
+ *  верхняя папка это решение
+ *  
+ *  решение или solution внутри себя содержит проект. проект это папка рядом с файлом sln. могут называться по разному.
+ *  
+ *  если в решение добавить новый проект, то в solution excplorer появится новый проект. и также появится папка в системе рядом с sln файлом.
+ */
+namespace Lesson10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Lessons10!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ csharp/Lesson10/My_project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace My_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from My_project!");
+            Console.ReadLine();
+        }
+    }
+}
