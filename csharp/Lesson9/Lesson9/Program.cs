458338f3fa50485c847a149c20b26fb2fe2b5334
Anton Mefodys <mefody123@gmail.com>
2021-11-30 21:15:35 +0300

    9

---
+++ csharp/Lesson9/Lesson9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1 = 2 + 2 * 2;
+            int result2 = (2 + 2) * 2;
+            int result3 = 5 * 4 / 2;
+        }
+    }
+}
