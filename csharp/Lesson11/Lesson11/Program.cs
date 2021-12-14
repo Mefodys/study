9c77561bb5d3ed5e4851f50f2762156759d07f40
Anton Mefodys <mefody123@gmail.com>
2021-12-14 21:41:45 +0300

    11

---
+++ csharp/Lesson11/Lesson11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            int a = 0;
+            a++;
+            Console.WriteLine(a++);
+            Console.WriteLine(--a);
+            Console.WriteLine(a);
+        }
+    }
+}
