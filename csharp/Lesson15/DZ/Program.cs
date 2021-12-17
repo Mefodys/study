c43468cb6209f3e6cfa2e642ddfaae30a4f2baea
Anton Mefodys <mefody123@gmail.com>
2021-12-17 21:46:17 +0300

    15

---
+++ csharp/Lesson15/DZ/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //calculator using switch
+            Console.WriteLine("vvedite pervuiu cifru ");
+            int chifra1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("vvedite vtoruiu cifru ");
+            int chifra2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("vvedite operaciu (+ - * /): ");
+            string oper = Console.ReadLine();
+
+            switch (oper)
+            {
+                case "+":
+                    Console.WriteLine(chifra1 + chifra2);
+                    break;
+                case "-":
+                    Console.WriteLine(chifra1 - chifra2);
+                    break;
+                case "*":
+                    Console.WriteLine(chifra1 * chifra2);
+                    break;
+                case "/":
+                    Console.WriteLine(chifra1 / chifra2);
+                    break;
+
+            }
+
+
+
+            //calculator using if else
+            Console.WriteLine("vvedite pervuiu cifru ");
+            int chifra3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("vvedite vtoruiu cifru ");
+            int chifra4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("vvedite operaciu (+ - * /): ");
+            string opera = Console.ReadLine();
+
+            if (opera == "+")
+            {
+                Console.WriteLine(chifra3 + chifra4);
+            }
+            else if (opera == "-")
+            {
+                Console.WriteLine(chifra3 - chifra4);
+            }
+            else if (opera == "*")
+            {
+                Console.WriteLine(chifra3 * chifra4);
+            }
+            else if (opera == "/")
+            {
+                Console.WriteLine(chifra3 / chifra4);
+            }
+            else
+            {
+                Console.WriteLine(chifra3 + chifra4);
+                Console.WriteLine(chifra3 - chifra4);
+                Console.WriteLine(chifra3 * chifra4);
+                Console.WriteLine(chifra3 / chifra4);
+
+            }
+        }
+    }
+}
