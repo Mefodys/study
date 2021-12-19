32b0ab7e94ec690d31391e5a25ac9090d73b3456
Anton Mefodys <mefody123@gmail.com>
2021-12-19 18:43:17 +0300

    16_DZ

--- csharp/Lesson16/Lesson16/Program.cs
+++ csharp/Lesson16/Lesson16/Program.cs
@@ -29,30 +29,102 @@ namespace Lesson16
 
             */
             int firstDigit;
+            int secondDigit;
+            string action;
 
             while (true)
             {
+                Console.Clear();
 
-                try
+                while (true)
                 {
-                    Console.Clear();
-                    Console.WriteLine("Enter the first digit: ");
-                    firstDigit = int.Parse(Console.ReadLine());
-                }
-                catch (Exception)
-                {
-                    //throw;
-                    Console.WriteLine("Input string was not in a correct format. Press Enter");
-                    Console.ReadLine();
-                    continue;
+                    try
+                    {
+                        //Console.Clear();
+                        Console.WriteLine("Enter the first digit: ");
+                        firstDigit = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        //throw;
+                        Console.WriteLine("Input string was not in a correct format. Press Enter");
+                        Console.ReadLine();
+                        continue;
+                    }
                 }
 
                 while (true)
                 {
-                    Console.WriteLine(firstDigit);
+                    try
+                    {
+                        Console.WriteLine("Enter the second digit: ");
+                        secondDigit = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        //throw;
+                        Console.WriteLine("Input string was not in a correct format. Press Enter");
+                        Console.ReadLine();
+                        continue;
+                    }
                 }
+
+                uint counter = 0;
+                while (counter <= 3)
+                {
+                    
+                    try
+                    {
+                        Console.WriteLine("Enter the action ('+' or '-' or '*' or '/') : ");
+                        action = Console.ReadLine();
+                        switch (action)
+                        {
+                            case "+":
+                                Console.Write(firstDigit + " + " + secondDigit + " = ");
+                                Console.Write(firstDigit + secondDigit + "\n");
+                                break;
+                            case "-":
+                                Console.Write(firstDigit + " - " + secondDigit + " = ");
+                                Console.Write(firstDigit - secondDigit + "\n");
+                                break;
+                            case "*":
+                                Console.Write(firstDigit + " * " + secondDigit + " = ");
+                                Console.Write(firstDigit * secondDigit + "\n");
+                                break;
+                            case "/":
+                                Console.Write(firstDigit + " / " + secondDigit + " = ");
+                                Console.Write(firstDigit / secondDigit + "\n");
+                                break;
+                            default:
+                                Console.WriteLine(" You entered not '+' or '-' or '*' or '/'! ");
+                                continue;
+                        }
+                        counter++;
+                    }
+                    catch (Exception)
+                    {
+                        //throw;
+                        Console.WriteLine("Input string was not in a correct format. Press Enter");
+                        Console.ReadLine();
+                        continue;
+                    }
+                    Console.WriteLine("Press Enter to restart...");
+
+                    Console.ReadLine();
+                    break;
+                }
+                
+
             }
-            
+
+
+
+
+
+
+
 
         }
     }
