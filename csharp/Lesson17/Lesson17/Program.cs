898db2748800882a96cdf86e844037cb9af3eda4
Anton Mefodys <mefody123@gmail.com>
2021-12-20 22:31:40 +0300

    17 do while

---
+++ csharp/Lesson17/Lesson17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Hello World! from DO WHILE");
+                
+
+            } while (false);
+
+            int count = 5;
+            
+            //*****************************
+            while (count < 5)
+            {
+                count++;
+                Console.WriteLine("Hello World! FROM WHILE1");
+            }
+
+            int b = 0;
+            do
+            {
+                b++;
+                Console.WriteLine("Hello World! FROM WHILE2");
+
+            } while (b < 3);
+
+        }
+    }
+}
