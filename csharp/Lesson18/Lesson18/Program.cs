c10727f54880dff661985b7c00cddd8df0d9b88a
Anton Mefodys <mefody123@gmail.com>
2021-12-20 23:09:38 +0300

    18 for

---
+++ csharp/Lesson18/Lesson18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a digit");
+            int limit = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= limit; i++)
+            {
+                Console.WriteLine("Hello World from FOR! " + i);
+
+            }
+
+/*            while (true)
+            {
+                Console.WriteLine("Hello World from WHILE!");
+            }
+*/
+
+        }
+    }
+}
---
+++ csharp/Lesson18/same_with_while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace same_with_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a digit");
+            int limit = int.Parse(Console.ReadLine());
+            int start = 1;
+
+            while (start <= limit)
+            {
+                Console.WriteLine("Hello World from WHILE! " + start);
+                start++;
+
+            }
+        }
+    }
+}
