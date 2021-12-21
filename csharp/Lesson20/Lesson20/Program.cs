ecf8972c84fddbfdd9abae8f466ac2f882fdeb23
Anton Mefodys <mefody123@gmail.com>
2021-12-21 19:16:12 +0300

    20 break

---
+++ csharp/Lesson20/Lesson20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 999; i++)
+            {
+                
+                Console.WriteLine(i);
+                if (i==10)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ csharp/Lesson20/Lesson20_msg/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson20_msg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 999; i++)
+            {
+                string msg = Console.ReadLine();
+                if (msg == "exit")
+                {
+                    Console.WriteLine("bye");
+                    System.Threading.Thread.Sleep(500);
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
