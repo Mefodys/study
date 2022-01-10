fc16638507f898e72296547b5ccbf6ec000efda4
Anton Mefodys <mefody123@gmail.com>
2022-01-10 22:06:15 +0300

    26

---
+++ csharp/Lesson26/Lesson26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 10, 3, 2, 55 };
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+         
+            Console.ReadLine();
+        }
+    }
+}
+
+//задания
+// 1 заполнить массив с клавиатуры
+// 2 вывести массив в обртном поярдке
+// 3 найти сумму четных чисел в массиве
+// 4 найти наименьшее число в массиве.
