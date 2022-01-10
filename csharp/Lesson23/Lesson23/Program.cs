f3041a8d517129375ade97802813d8a3a57f5e00
Anton Mefodys <mefody123@gmail.com>
2022-01-10 16:20:29 +0300

    lesson23

---
+++ csharp/Lesson23/Lesson23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool accessAllowed;
+            string storedPassword = "qwerty";
+            string enteredPassword = Console.ReadLine();
+            
+           /* if (enteredPassword == storedPassword)
+            {
+                accessAllowed = true;
+
+            }
+            else
+            {
+                accessAllowed = false;
+
+            *///}//
+
+                accessAllowed = enteredPassword == storedPassword ? true : false;
+                Console.WriteLine(accessAllowed);
+
+        }
+    }
+}
