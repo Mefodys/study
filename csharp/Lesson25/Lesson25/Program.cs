f948cf86e173591055fe8a8bb56d6519f7d24ae5
Anton Mefodys <mefody123@gmail.com>
2022-01-10 21:54:37 +0300

    25

---
+++ csharp/Lesson25/Lesson25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] myArray = new int[5]; //массив нулей типа  int 
+
+            int[] myArray2 = new int[5] {100, 1, 2, 3, 4 }; //инициализируем массив значениями
+
+            int[] myArray3 = new int[] { 100, 1, 2, 3, 10, 50 }; //инициализируем массив значениями,не указываем количество элементов
+
+            int[] myArray4 = new [] { 100, 1, 2, 3, 10, 50 }; //инициализируем массив значениями, не испльзуем указание типа после new
+
+            int[] myArray5 = { 100, 1, 2, 3, 4 }; //инициализируем массив значениями, упрощенно.
+
+
+            int[] myArray6 = Enumerable.Repeat(5, 10).ToArray();
+
+            int[] myArray7 = Enumerable.Range(4, 5).ToArray();
+
+
+
+            Console.WriteLine("Hell0");
+            Console.ReadLine();
+        }
+    }
+}
