745b789e981033ed89765cc29ccd5cb1f284d493
Anton Mefodys <mefody123@gmail.com>
2022-01-11 21:06:51 +0300

    29

---
+++ csharp/Lesson29/Lesson29/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //теория по двумерным массивам.
+            
+            //прямоульный массив - число строк равно числу столбцов
+
+            //зубчатый массив - число строк не равно числу столбцов
+        }
+    }
+}
