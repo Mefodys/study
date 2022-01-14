a2c538f55e9e1b2c93a6a2dea2b7c0dab85e62b5
Anton Mefodys <mefody123@gmail.com>
2022-01-14 16:49:24 +0300

    41 ??=

---
+++ csharp/Lesson41/Lesson41/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+
+            //string result = str ?? string.Empty; //в str остается null, в result пустая строка "".
+            string result = str ??= string.Empty; //в str также теперь содержится пустая строка "", в result пустая строка "".
+
+
+            Console.WriteLine(result.Length);
+            Console.WriteLine(str.Length); // в случае ??= мы можем работать далее с str.
+
+        }
+    }
+}
---
+++ csharp/Lesson41/Lesson41_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson41_1
+{
+    class Program
+    {
+        static int [] GetArray()
+        {
+            int[] arr = null;
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] arr = GetArray();
+
+            arr ??= new int[0];
+        }
+    }
+}
