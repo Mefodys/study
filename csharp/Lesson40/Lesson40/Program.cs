ac30f5057940cd819377c3e6b0d03cebcc25d973
Anton Mefodys <mefody123@gmail.com>
2022-01-14 16:37:09 +0300

    40 ??

---
+++ csharp/Lesson40/Lesson40/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "test";
+            Console.WriteLine(str);
+
+            string str2 = null;
+            Console.WriteLine(str2); //ничего не выведется. но нам нужно обработать этот момент.
+
+            if (str2 == null)
+            {
+                Console.WriteLine("no data");
+            }
+            else
+            {
+                Console.WriteLine(str2);
+            }
+
+            string str3 = null;
+            //string str3 = "test";
+
+            Console.WriteLine(str3 ?? "no data (with ??)");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp/Lesson40/Lesson40_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson40_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "test";
+            str = null;
+
+//            string result = str ?? "";
+            string result = str ?? string.Empty;
+
+
+            //Console.WriteLine("numbers of symbols in the string is " + str.Length);
+            Console.WriteLine("numbers of symbols in the string is " + result.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
