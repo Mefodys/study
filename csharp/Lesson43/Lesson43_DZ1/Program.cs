f9deb34a13f31420229bade7f301046273a73375
Anton Mefodys <mefody123@gmail.com>
2022-01-17 18:05:01 +0300

    43 DZ1

---
+++ csharp/Lesson43/Lesson43_DZ1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.  написать метод Resize изменяющий количество элементов  массива
+//(метод должен иметь возможность увеличить или уменьшить количетво элементов массива)
+
+namespace Lesson43_DZ1
+{
+    class Program
+    {
+        static void Resize(int[] arr, uint count)
+        {
+            var tempArr = new int[arr.Length + count];
+
+            arr.CopyTo(tempArr, 0);
+            
+
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 10, 5, 3 };
+
+            Resize(myArray, 2);
+        }
+    }
+}
